9d5ec5549985ad9e86ebc882e877e4ac1d62a419
olivrcarol <carolina.opereira@aluno.edu.es.gov.br>
2025-09-03 11:58:56 -0300

    Create index.php

---
+++ aula 01-3 trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> aula 01 php </title>
+    </head>
+    <body>
+        <?php
+        echo"hello world";
+        echo"Gaby e Carol união punho cruzado";
+        ?>
+    </body>
+</html>
