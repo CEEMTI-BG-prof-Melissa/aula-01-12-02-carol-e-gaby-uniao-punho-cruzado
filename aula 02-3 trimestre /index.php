7e5ccbdfe0977356fb66e20c98e849e588fe7500
olivrcarol <carolina.opereira@aluno.edu.es.gov.br>
2025-09-03 11:59:56 -0300

    Create index.php

---
+++ aula 02-3 trimestre /index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>  Aula 02 </title>
+    </head>
+    <body>
+        <?php
+            $nome1="gabrielly";
+            $nome2="carolina";
+
+             echo "meu nome é".$nome1 ."e minha dupla é" .$nome2;
+        ?>
+    </body>
+</html>
