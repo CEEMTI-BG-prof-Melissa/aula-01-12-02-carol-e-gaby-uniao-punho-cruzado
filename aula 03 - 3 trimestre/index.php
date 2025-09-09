3acc5d28a1843b618df04c687f059b970d8ef293
olivrcarol <carolina.opereira@aluno.edu.es.gov.br>
2025-09-09 11:56:13 -0300

    Create index.php

---
+++ aula 03 - 3 trimestre/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>  Aula 03 - PHP </title>
+    </head>
+
+    <body>
+        <?php
+            $nome1="gabrielly";
+            $nome2="carolina";
+
+            $n1=10;
+            $n2=2;
+
+            $soma=$n1+$n2;
+
+            echo "meu nome é ".$nome1 ." e minha dupla é " .$nome2;
+            echo "<br>meu nome é $nome1 e minha dupla é $nome2";
+            echo "<br><br>a soma de $n1 com $n2 é igual a $soma.";
+        ?>
+    </body>
+</html>
